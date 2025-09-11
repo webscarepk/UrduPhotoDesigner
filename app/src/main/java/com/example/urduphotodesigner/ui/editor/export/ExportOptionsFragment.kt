6c20356acf3d9efefa4c9513984b2c828f3822ca
Sumaib Hamza <89627208+SumaibH@users.noreply.github.com>
2025-09-11 12:38:19 +0500

    latest

--- app/src/main/java/com/example/urduphotodesigner/ui/editor/export/ExportOptionsFragment.kt
+++ app/src/main/java/com/example/urduphotodesigner/ui/editor/export/ExportOptionsFragment.kt
@@ -84,6 +84,9 @@ class ExportOptionsFragment : BottomSheetDialogFragment() {
                     viewModel.exportOptions.value!!.copy(format = selected)
                 )
             }
+            binding.options.postDelayed({
+                dismiss()
+            }, 500)
         }
 
         binding.options.adapter = adapter
